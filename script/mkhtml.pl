8fe9a2120e786c28dfdde0230a52b7ba6dd71b9b
kurema <kurema_makoto_software@yahoo.co.jp>
2019-07-31 21:31:54 +0900

    script

---
+++ script/mkhtml.pl
@@ -0,0 +1,90 @@
+#/usr/bin/perl
+use utf8;
+
+my %words=GetHtmlHash();
+
+if(@ARGV == 1){
+  open(my $F, "<", $ARGV[0]) or die;
+  my $text = do { local $/; <$F> };
+  print TbHtml($text);
+}
+
+sub TbHtml{
+  my ($text)=@_;
+  my $result="";
+  
+  my $block="";
+  my $block_closer="";
+  my $para_opner="";
+  my $para_closer="";
+  my $line_start="";
+  
+  my @lines=split(/\n/, $text);
+  foreach my $line(@lines){
+    if($line=~ /^\(\*(\w+)\*\)$/){
+      #ブロック種類指定は"(*type*)"で行う。
+      my $class=$1;
+      $result.=$block.$para_closer.$block_closer;
+      $block=$words{"block_open"};
+      $block=~ s/\[CLASS\]/$class/g;
+      $para_opner=$words{"block_para_open"};
+      $para_closer=$words{"block_para_close"};
+      $block_closer=$words{"block_close"};
+      $line_start=$para_opner;
+    }elsif($line=~ /^(\#{1,6})/){
+      #見出しはマークダウン相当
+      my $level=length($1);
+      $line=~ s/^\#{1,6}\s*//;
+      $result.=$block.$para_closer.$block_closer;
+      $result.=$words{"h".$level."_open"}.$line.$words{"h".$level."_close"};
+      $para_opener=$words{"content_open"};
+      $para_opener=~ s/\[CLASS\]/level_$level/g;
+      $para_closer=$words{"content_close"};
+      $line_start=$para_opener;
+      $block="";
+      $block_closer="";
+    }elsif($line=~ /^\s*$/){
+      $line_start=$para_closer.$para_opener;
+    }else{
+      $block.=$line_start.TextDecorate($line);
+      $line_start=$words{"enter"};
+    }
+  }
+  $result.=$block.$para_closer.$block_closer;
+  return $result;
+}
+
+sub TextDecorate{
+  my ($text)=@_;
+  $text=~ s/\*\*([^\*]+)\*\*/$words{'decoration2_open'}$1$words{'decoration2_close'}/g;
+  $text=~ s/\*([^\*]+)\*/$words{'decoration1_open'}$1$words{'decoration1_close'}/g;
+  return $text;
+}
+
+sub GetHtmlHash{
+  return (
+    'enter' => "\n",
+    'block_open' => '<div class="[CLASS]">',
+    'block_close' => "</div>\n",
+    'block_para_open' => '<p>',
+    'block_para_close' => "</p>\n",
+    'h1_open' => '<h1>',
+    'h1_close' => "</h1>\n",
+    'h2_open' => '<h2>',
+    'h2_close' => "</h2>\n",
+    'h3_open' => '<h3>',
+    'h3_close' => "</h3>\n",
+    'h4_open' => '<h4>',
+    'h4_close' => "</h4>\n",
+    'h5_open' => '<h5>',
+    'h5_close' => "</h5>\n",
+    'h6_open' => '<h6>',
+    'h6_close' => "</h6>\n",
+    'content_open' => "<p class='[CLASS]'>",
+    'content_close' => "</p>\n",
+    'decoration1_open' => '<span class="decoration1">',
+    'decoration1_close' => "</span>",
+    'decoration2_open' => '<span class="decoration2">',
+    'decoration2_close' => '</span>'
+  );
+}

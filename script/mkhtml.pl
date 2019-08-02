c3d18178e6f4f3f95666b0463a41031d96dd3dbe
kurema <kurema_makoto_software@yahoo.co.jp>
2019-08-03 00:47:08 +0900

    daily update
    そこそこ記事は進んだ。
    ちなみにWSLのnanoで編集してたりします。
    カーソルが消える謎バグがある。
    
    そういやtocは上手な方法を考えないとな。

--- script/mkhtml.pl
+++ script/mkhtml.pl
@@ -22,6 +22,9 @@ sub TbHtml{
   my $line_start="";
   my $line_end="";
   
+  my $list_opener="";
+  my $list_closer="";
+  
   my @lines=split(/\n/, $text);
   foreach my $line(@lines){
     if($line=~ /^\(\*\*(\w+)\*\*\)$/){
@@ -34,6 +37,7 @@ sub TbHtml{
       $para_closer=$words{"para_special_close"};
       $line_start=$para_opener;
       $line_end="";
+      $list_opener="";
     }elsif($line=~ /^\(\*(\w+)\*\)$/){
       #ブロック種類指定は"(*type*)"で行う。
       my $class=$1;
@@ -45,6 +49,7 @@ sub TbHtml{
       $block_closer=$words{"block_close"};
       $line_start=$para_opener;
       $line_end="";
+      $list_opener="";
     }elsif($line=~ /^(\#{1,6})/){
       #見出しはマークダウン相当
       my $level=length($1);
@@ -58,10 +63,34 @@ sub TbHtml{
       $block="";
       $block_closer="";
       $line_end="";
+      $list_opener="";
     }elsif($line=~ /^\s*$/){
       $block.=$line_end;
       $line_start=$para_opener;
       $line_end="";
+      $list_opener="";
+    }elsif($line=~ /^\s*\*\s+/){
+      $line=~ s/^\s*\*\s+//g;
+      if($list_opener ne ""){
+        $block.=$list_opener.$line.$list_closer;
+      }else{
+        $list_opener=$words{"ul_li_open"};
+        $list_closer=$words{"ul_li_close"};
+        $line_end=$words{"ul_close"};
+        $line_start="";
+        $block.=$words{"ul_open"}.$list_opener.$line.$list_closer;
+      }
+    }elsif($line=~ /^\s*\d+\.\s+/){
+      $line=~ s/^\s*\d+\.\s+//g;
+      if($list_opener ne ""){
+        $block.=$list_opener.$line.$list_closer;
+      }else{
+        $list_opener=$words{"ol_li_open"};
+        $list_closer=$words{"ol_li_close"};
+        $line_end=$words{"ol_close"};
+        $line_start="";
+        $block.=$words{"ol_open"}.$list_opener.$line.$list_closer;
+      }
     }else{
       $block.=$line_start.TextDecorate($line);
       if($line=~ /\s{2}$/){
@@ -71,9 +100,10 @@ sub TbHtml{
       }
       
       $line_end=$para_closer;
+      $list_opener="";
     }
   }
-  $result.=$block.$para_closer.$block_closer;
+  $result.=$block.$line_end.$block_closer;
   return $result;
 }
 
@@ -111,7 +141,15 @@ sub GetHtmlHash{
     'decoration1_open' => '<span class="decoration1">',
     'decoration1_close' => "</span>",
     'decoration2_open' => '<span class="decoration2">',
-    'decoration2_close' => '</span>'
+    'decoration2_close' => '</span>',
+    'ul_open' => "\n<ul>\n",
+    'ul_close' => "</ul>\n",
+    'ol_open' => "\n<ol>\n",
+    'ol_close' => "</ol>\n",
+    'ul_li_open' => "  <li>",
+    'ul_li_close' => "</li>\n",
+    'ol_li_open' => "  <li>",
+    'ol_li_close' => "</li>\n",
   );
 }
 
@@ -122,12 +160,10 @@ sub GetHtmlHeader{
   <head>
     <style>
 div.question p.q:before, div.question p.a:before, div.question p.e:before{
-  display: -webkit-flex;
   display: flex;
-  -webkit-align-items: center;
   align-items: center;
-  -webkit-justify-content: center;
   justify-content: center;
+
   height :2em;
   border-radius: 1em;
   text-align: center;
@@ -144,6 +180,15 @@ div.question p.e:before{
 }
 div.question p.e, div.question p.e_noheader{
 }
+div.question:before, div.question:after{
+  content:"";
+  
+  display: block;
+  overflow: hidden;
+  border-style: dotted;
+  border-width: 3px;
+  border-color: #f08c32;
+}
 span.decoration1{
   font-weight: bold;
 }
@@ -153,6 +198,20 @@ span.decoration2{
 
 body{
   counter-reset: chap-h1 chap-h2 chap-h3;
+  margin: 3em;
+}
+
+div.chapter_title{
+  counter-reset: chap-h2 chap-h3;
+}
+div.chapter_title p.title{
+  font-weight:bold;
+  font-size:1.8em;
+}
+div.chapter_title p.title:before{
+  counter-increment: chap-h1;
+  content: "第" counter(chap-h1) "章";
+  margin-right: 0.5em;
 }
 
 h1{
@@ -164,15 +223,20 @@ h1:before{
   margin-right: 0.5em;
 }
 h2{
+  display: flex;
+  align-items: center;
+  justify-content: left;
+
   counter-reset: chap-h3;
-  display: block;
   background: #f08c32;
   height:1.8em;
+  color:white;
 }
 h2:before{
   counter-increment: chap-h2;
   content: counter(chap-h1) "-" counter(chap-h2);
   margin-right: 0.5em;
+  margin-left: 0.5em;
 }
 h3{
 }
@@ -196,7 +260,7 @@ h4{
 h4:before{
   width:1em;
   height:1em;
-  border-radius: 0.1em;
+  border-radius: 0.2em;
   border-width:0.1em;
   border-color: #c85a00;
   background: #f08c32;
@@ -212,6 +276,7 @@ p.caption{
   border-color:black;
   border-style:dotted;
   border-width:1px;
+  padding:0.5em;
 }
 p.caption:before{
   content: "補遺";
@@ -221,8 +286,48 @@ p.caption:before{
 p.desc{
   font-weight: bold;
 }
+
+div.remember:before{
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  padding: 0 1em;
+
+  background: #f08c32;
+  color: white;
+  content: "覚えよう！";
+}
+div.remember{
+  background: #fadcbe;
+  font-weight: bold;
+  margin:1em 0;
+  padding:0 0 0.5em 0;
+}
+div.remember ul{
+  list-style-type: square;
+}
+
+div.column:before{
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  padding: 0 0.5em;
+
+  content: "コラム";
+  font-weight: bold;
+}
+
+div.column{
+  background: #fadcbe;
+  border-color: #f08c32;
+  border-radius: 1em;
+  
+  padding: 0.5em;
+  magrin: 0.5em;
+}
     </style>
   </head>
   <body>
 EOL
 }
+
